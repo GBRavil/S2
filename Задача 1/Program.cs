88a7be780e37f7b8be0bd2ab9dd8501c272eea6c
Ravil <lllx2@yandex.ru>
2022-10-09 18:00:25 +0300

    1

--- Задача 1/Program.cs
+++ Задача 1/Program.cs
@@ -2,10 +2,22 @@
 Console.WriteLine("Введите номер дня недели");
 String s1 = Console.ReadLine();
 int number = Convert.ToInt32(s1);
-if(number == week[5] || number == week[6]){      
-    Console.WriteLine("Введеный день недели выходной");
-} else if(number > week[6]) {
-    Console.WriteLine("Нет такого дня недели");
-} else {
-    Console.WriteLine("Введеный день недели не выходной");
+// if(number == week[5] || number == week[6]){      
+//     Console.WriteLine("Введеный день недели выходной");
+// } else if(number > week[6]) {
+//     Console.WriteLine("Нет такого дня недели");
+// } else {
+//     Console.WriteLine("Введеный день недели не выходной");
+// }
+void Weekend (int[] w, int n){
+    int length = w.Length;
+    int i = 0;
+    if (n == w[5] || n == w[6]) {
+        Console.WriteLine("Введеный день недели выходной");
+    } else if(n > w[6]) {
+        Console.WriteLine("Нет такого дня недели");
+    } else {
+        Console.WriteLine("Введеный день недели не выходной");
+    }    
 }
+Weekend(week, number);
---
+++ Задача 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число!");
+String s1 = Console.ReadLine();
+int i1 = Convert.ToInt32(s1);
+i1 = i1*i1;
+Console.WriteLine("Введите второе число!");
+String s2 = Console.ReadLine();
+int i2 = Convert.ToInt32(s2);
+if(i1==i2) {
+    Console.WriteLine("Первое число является квадратом второго числа");
+} else {
+    Console.WriteLine("Первое число не является квадратом второго числа");
+} 
