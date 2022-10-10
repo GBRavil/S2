9d39ea07fdf669d4dd8ac5ae219c1856c282244a
Ravil <lllx2@yandex.ru>
2022-10-10 23:37:24 +0300

    5

---
+++ Задча 5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите значение х");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение y");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение z");
+int z = Convert.ToInt32(Console.ReadLine());
+
+void A2d(int a, int b){
+    
+}
