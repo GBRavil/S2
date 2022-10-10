f4080487d8747c5df18705530967e043c469a844
Ravil <lllx2@yandex.ru>
2022-10-10 21:21:08 +0300

    3

---
+++ Задача 3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Задайте номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+Range(number);
+void Range (int n) {
+    if(n==1) {
+        Console.WriteLine("Диапазон первой координатной четверти x > 0 и y > 0");
+    } else if(n==2) {
+        Console.WriteLine("Диапазон второй коорлинатной четверти x < 0 и y < 0");
+    } else if(n==3) {
+        Console.WriteLine("Диапазон третьей координатной четверти x > 0 и y < 0");
+    } else if(n==4) {
+        Console.WriteLine("Диапазон четвертой координатной четверти x < 0 и y > 0");
+    }
+}
