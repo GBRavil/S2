ec32011f92d20cc0f205492e55a39a530765fb39
Ravil <lllx2@yandex.ru>
2022-10-10 21:53:51 +0300

    4

---
+++ Задача 4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+Check(number);
+void Check(int n) {
+    if (n%10 == n/10000%10 && n/10%10 == n/1000%10) {
+        Console.WriteLine("Число прошло проверку на палиндромом");
+    } else {
+        Console.WriteLine("Число не прошло проверку на палиндромом");
+    }
+}
