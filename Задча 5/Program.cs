015d0875cc90fb7e5480ebb673f273beab55f5c0
Ravil <lllx2@yandex.ru>
2022-10-11 21:02:23 +0300

    5

--- Задча 5/Program.cs
+++ Задча 5/Program.cs
@@ -1,10 +1,19 @@
-﻿Console.WriteLine("Введите значение х");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите значение y");
-int y = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите значение z");
-int z = Convert.ToInt32(Console.ReadLine());
-
-void A2d(int a, int b){
-    
+﻿Random rnd = new Random ();
+int[] r2d = new int[4];
+for (int i = 0; i < r2d.Length; i++) {
+    r2d[i] = rnd.Next (-10, 10);
 }
+double A = Math.Pow((r2d[2]-r2d[0]), 2); 
+double B = Math.Pow((r2d[3]-r2d[1]), 2);
+double AB = Math.Sqrt((A+B));
+Console.WriteLine("Расстояние между точками в пространстве 2D "+AB);
+
+int[] r3d = new int[6];
+for (int i = 0; i < r3d.Length; i++) {
+    r3d[i] = rnd.Next (-10, 10);
+}
+double AC = Math.Pow((r3d[3]-r3d[0]), 2); 
+double BC = Math.Pow((r3d[4]-r3d[1]), 2);
+double CC = Math.Pow((r3d[5]-r3d[2]), 2);
+double ABC = Math.Sqrt((AC+BC+CC));
+Console.WriteLine("Расстояние между точками в пространстве 3D "+ABC);
